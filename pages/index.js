e63d42cf452a0ce653d0a793d8f72eee741c7807
PalakKumari <palakkumari404@gmail.com>
2021-12-29 20:44:08 +0530

    changed navbar folder.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from '../components/navbar/Navbar'
+import Navbar from '../src/components/navbar/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
--- src/components/nav.js
+++
@@ -1,1 +0,0 @@
-//create a navbar component 
