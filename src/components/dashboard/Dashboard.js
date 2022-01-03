84d67040a8e84115b2db0d9f7f2c5028eb9c2e97
shivam tyagi <shivamt.ug19.ec@nitp.ac.in>
2022-01-03 23:06:19 +0530

    dynamic props

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,46 +1,66 @@
+import styles from "./Dashboard.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
-import styles from "./Dashboard.module.css"
-
-export default function Dashboard() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.profile_card_container}>
-                <div className={styles.profile_image_container}>
-                    <div>
-                        <img src="/picture.png" />
-                        {/* <img src="/Ellipse 13.png" /> */}
-                    </div>
-                </div>
-                <div className={styles.profile_description}>
-                    <p>NAME:<span>EMANUAEL FRANK</span></p>
-                    <p>EMail</p>
-                </div>
-                <div className={styles.profile_stats}>
-                    <div className={styles.profile_stat}>
-                        <p>Location</p>
-                        <p>Roorkee</p>
-                    </div>
-                    <div className={styles.profile_stat}>
-                        <p>Your Score</p>
-                        <p>8</p>
-                    </div>
-                    <div className={styles.profile_stat}>
-                        <p>Phone Number</p>
-                        <p>343-34-432</p>
-                    </div>
-                    <div className={styles.profile_stat}>
-                        <p>Date of Last Report </p>
-                        <p>3-4-12</p>
-                    </div>
-
-                    <button className={styles.calculate}>CALCULATE</button>
-
-                </div>
-            </div>
-            <div className={styles.statistics}>
-                <h1>Your STATS</h1>
-            </div>
+export default function Dashboard(props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.profile_card_container}>
+        <div className={styles.profile_image_container}>
+          <Image
+            className={styles.profile_image}
+            src={`/images/dashboard/picture.png`}
+            alt="Profile picture"
+            height={10}
+            width={10}
+            layout="responsive"
+          />
+          <div className={styles.profile_description}>
+            <p>
+              NAME:<span>{props.name}</span>
+            </p>
+            <p>
+              EMail:<span>{props.email}</span>
+            </p>
+          </div>
         </div>
 
-    )
+        <div className={styles.profile_stats}>
+          <div className={styles.profile_stat}>
+            <p>Location</p>
+            <p>{props.location}</p>
+          </div>
+          <div className={styles.profile_stat}>
+            <p>Your Score</p>
+            <p>{props.score}</p>
+          </div>
+          <div className={styles.profile_stat}>
+            <p>Phone Number</p>
+            <p>{props.phone}</p>
+          </div>
+          <div className={styles.profile_stat}>
+            <p>Date of Last Report </p>
+            <p>{props.date}</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.statistics}>
+        <h1>Your STATS
+          <span>
+          <Image  className={styles.print}
+            src={`/images/dashboard/human-footprints.png`}
+            alt=" picture"
+            height={60}
+            width={40}
+           
+          />
+          </span>
+        </h1>
+      
+        <Link href="/" passHref>
+          <span className={styles.button}>Calculate now &rarr;</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
