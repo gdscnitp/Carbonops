519a1d6e910ec4b974be34cec0e36a3928f73c86
Vivek Ankit <72314052+vian9@users.noreply.github.com>
2022-01-04 01:01:19 +0530

    initial commit

---
+++ pages/signupin.js
@@ -0,0 +1,19 @@
+import Navbar from "../src/components/navbar/Navbar";
+import Sign from "../src/components/sign/sign";
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Signu.module.css";
+
+export default function SignUpIn() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Environment Awareness App</title>
+        <meta name="description" content="" />
+      </Head>
+
+      <Navbar action1="" action2="" buttonText="" />
+      <Sign text1="an User" text2="Date of Birth" />
+    </div>
+  );
+}
