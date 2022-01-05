2de3a8f92d46f8c08c16cdb21eaf3ab915188279
amisha <amishab.ug20.ce@nitp.ac.in>
2022-01-05 16:56:59 +0530

    all required changes are done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Navbar from '../src/components/navbar/Navbar'
 import styles from '../styles/Home.module.css'
+
+import Login from '../src/components/LoginPage/loginPage'
 export default function Home() {
   return (
      
@@ -9,7 +11,7 @@ export default function Home() {
         <title>Environment Awareness App</title>
         <meta name="description" content="" />
       </Head>
-
+     <Login />
     </div>
   )
 }
