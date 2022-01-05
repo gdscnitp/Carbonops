037c800903653a2a3397de768fd20f2c53b20b20
Amisha Bharti <amishab.ug20.ce@nitp.ac.in>
2022-01-05 17:25:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,12 @@ import styles from '../styles/Home.module.css'
 import Login from '../src/components/LoginPage/loginPage'
 export default function Home() {
   return (
+     <div className={styles.container}>
       <Head>
         <title>Environment Awareness App</title>
         <meta name="description" content="" />
       </Head>
+     <Login />
     </div>
   )
 }
