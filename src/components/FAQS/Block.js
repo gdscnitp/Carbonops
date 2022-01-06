01deb5970c14023a5a9b876f30f7529beb7d410d
Kishan <76819584+KISHAN-del@users.noreply.github.com>
2022-01-06 14:47:44 +0530

    new modification has been done as suggested

--- src/components/FAQS/Block.js
+++ src/components/FAQS/Block.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
+var Questions = ["Question 1"];
+var Ans = ["Answer 1"];
 function Block(props) {
-  return (
-      <div className={props.class}>{props.text}</div>
-  );
+    const [clicked, setClicked] = useState(false);
+  function showAns(e) {
+    setClicked(!clicked);
+  }
+  var Unknown = clicked ? Ans[0] : Questions[0];
+  if (props.class === "main__options") {
+    return (
+      ( <div className={props.class} onClick={showAns}>{Unknown}</div> )
+    );
+  }
+  else{
+    return (
+      ( <div className={props.class}>{props.text}</div> )
+    );
+  }
 }
 
 export default Block;
