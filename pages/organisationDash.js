43fe0099e3ffcadba9f7783947d9ab91c7ee3fce
priyakumari02 <77202746+priyakumari02@users.noreply.github.com>
2022-01-07 07:27:22 +0530

    Updated Organisation Dash

--- pages/organisationDash.js
+++ pages/organisationDash.js
@@ -3,16 +3,26 @@ import Navbar from '../src/components/navbar/Navbar';
 import OrganisationDashBoard from '../src/components/organisation/Content'
 import styles from '../styles/Home.module.css'
 
+const organisationDetail={
+    profile:"Welcome to Green World",
+    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis reiciendis incidunt nostrum voluptates! Quidem excepturi nam optio rerum qui blanditiis iure,",
+    link:"http://mywebsite.com",
+    
+};
+
+
+
 const organisationDash = () => {
     return (
         <>
-        <Navbar action1="" action2="" buttonText="Create Events" />
+        {/* <Navbar action1="Events" action2="Dashboard" action3="Logout" buttonText="Create Events" /> */}
+        <Navbar action1="" action2="Logout" buttonText="Create Events" />
 
         <main className={styles.main}>
-        <OrganisationDashBoard />
+        <OrganisationDashBoard {...organisationDetail}/>
        </main> 
         </>
     )
 }
 
-export default organisationDash
+export default organisationDash;
--- src/components/organisation/Content.js
+++ src/components/organisation/Content.js
@@ -5,12 +5,11 @@ import Company from '/public/images/organisationDash/Logo.png';
 import Product from '/public/images/organisationDash/Product.png';
 import styles from './Content.module.css';
 
-function Content() {
+function Content(props) {
   return (
     <>
       <div className={styles.dashPage}>
-        <strong className={styles.top}>Organisation</strong>
-        <div className={styles.container}>
+        <div className={styles.container1}>
           <div className={styles.LeftPosition}>
             <div className={styles.img}>
               <Image
@@ -20,7 +19,6 @@ function Content() {
                 width={350}
                 height={300}
               />
-              <div className={styles.name}>companyName</div>
             </div>
           </div>
           <div className={styles.RightPosition}>
@@ -28,136 +26,120 @@ function Content() {
               <form action="">
                 <div>
                   <div className={styles.topic}>Your Profile Text here</div>
-                  <input
-                    type="profile"
-                    placeholder="Your Profile Text here"
-                    className={styles.input}
-                  />
+                  <p className={styles.text}>{props.profile}</p>
                 </div>
                 <div>
                   <div className={styles.topic}>Your Description</div>
-                  <textarea
-                    type="description"
-                    placeholder="Your Description"
-                    className={styles.input}
-                  />
+                  <p className={styles.text}>{props.description}</p>
                 </div>
                 <div>
                   <div className={styles.topic}>Your Website Link</div>
-                  <input
-                    type="website"
-                    placeholder="Your Website Link"
-                    className={styles.input}
-                  />
+                  <p className={styles.text}>{props.link}</p>
                 </div>
               </form>
             </div>
           </div>
-
-          <div className={styles.LeftItem}>
-            <div className={styles.organDetails}>
-              <strong className={styles.top}>Organisation Details</strong>
+        </div>
+        <div className={styles.container2}>
+          <div className={styles.organDetails}>
+            {/* <strong className={styles.top}>Organisation Details</strong>
               <br />
-              <br />
-              <p className={styles.topic}>Update Details</p>
-              <div>
-                <input
-                  type="name"
-                  placeholder="Organisation Name"
-                  className={styles.input}
-                />
-              </div>
-              <div>
-                <input
-                  type="type"
-                  placeholder="Organisation Type"
-                  className={styles.input}
-                />
-              </div>
+              <br /> */}
+            <p className={styles.topic}>Update Details</p>
+            <div>
+              <input
+                type="name"
+                placeholder="Organisation Name"
+                className={styles.input}
+              />
+            </div>
+            <div>
+              <input
+                type="type"
+                placeholder="Organisation Type"
+                className={styles.input}
+              />
+            </div>
 
-              <div>
-                <input
-                  type="location"
-                  placeholder="Organisation Location"
-                  className={styles.input}
-                />
-              </div>
+            <div>
+              <input
+                type="location"
+                placeholder="Organisation Location"
+                className={styles.input}
+              />
+            </div>
 
-              <p className={styles.topic}>Social Media Handles</p>
-              <div>
-                <input
-                  type="facebook"
-                  placeholder="Facebook"
-                  className={styles.input}
-                />
-              </div>
-              <div>
-                <input
-                  type="twitter"
-                  placeholder="Twitter"
-                  className={styles.input}
-                />
-              </div>
-              <div>
-                <input
-                  type="linkedin"
-                  placeholder="Linkedin"
-                  className={styles.input}
-                />
-              </div>
-              <div className={styles.alignCenter}>
-                <Link href=" ">
-                  <a className={styles.button}>Save</a>
-                </Link>
-              </div>
+            <p className={styles.topic}>Social Media Handles</p>
+            <div>
+              <input
+                type="facebook"
+                placeholder="Facebook"
+                className={styles.input}
+              />
+            </div>
+            <div>
+              <input
+                type="twitter"
+                placeholder="Twitter"
+                className={styles.input}
+              />
+            </div>
+            <div>
+              <input
+                type="linkedin"
+                placeholder="Linkedin"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.alignCenter}>
+              <Link href=" ">
+                <a className={styles.button}>Save</a>
+              </Link>
             </div>
           </div>
+        </div>
 
-          <div className={styles.RightItem}>
-            <div className={styles.organProducts}>
-              <strong className={styles.top}>Product Details</strong>
-              <br />
-              <br />
-              <div className={styles.img}>
-                <Image
-                  src={Product}
-                  placeholder="blur"
-                  alt="company logo"
-                  width={200}
-                  height={300}
-                />
-              </div>
-              <br />
-              <br />
-              <div className={styles.topic}>Product Name</div>
-              <input
-                type="name"
-                placeholder="Product Name"
-                className={styles.input}
+        <div className={styles.container2}>
+          <div className={styles.organProducts}>
+            <div className={styles.img}>
+              <Image
+                src={Product}
+                placeholder="blur"
+                alt="company logo"
+                width={200}
+                height={300}
               />
-              <div className={styles.topic}>Product Description</div>
-              <input
-                type="name"
-                placeholder="Product Description"
-                className={styles.input}
-              />
-              <div className={styles.topic}>Product URL</div>
-              <input
-                type="url"
-                placeholder="Product URL"
-                className={styles.input}
-              />
-              <div className={styles.topic}>Product Price</div>
-              <input
-                type="price"
-                placeholder="Product Price"
-                className={styles.input}
-              />
-              <div className={styles.alignCenter}>
-                <Link href=" ">
-                  <a className={styles.button}>Add</a>
-                </Link>
-              </div>
+            </div>
+            <br />
+            <br />
+            <div className={styles.topic}>Product Name</div>
+            <input
+              type="name"
+              placeholder="Product Name"
+              className={styles.input}
+            />
+           <div className={styles.topic}>Your Description</div>
+                  <textarea
+                    type="description"
+                    placeholder="Product Description"
+                    className={styles.input}
+                  />
+            <div className={styles.topic}>Product URL</div>
+            <input
+              type="url"
+              placeholder="Product URL"
+              className={styles.input}
+            />
+            <div className={styles.topic}>Product Price</div>
+            <input
+              type="price"
+              placeholder="Product Price"
+              className={styles.input}
+            />
+            <div className={styles.alignCenter}>
+              <Link href=" ">
+                <a className={styles.button}>Add</a>
+              </Link>
             </div>
           </div>
         </div>
