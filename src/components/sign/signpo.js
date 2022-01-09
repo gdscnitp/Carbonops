d17e33e3172a5796b04a4928bbee899542e1006e
Vivek Ankit <72314052+vian9@users.noreply.github.com>
2022-01-09 16:30:39 +0530

    initial commit

---
+++ src/components/sign/signpo.js
@@ -0,0 +1,43 @@
+import styles from "./Signpop.module.css";
+import React from "react";
+
+export default function Sign(props) {
+  return (
+    <div className={styles.container}>
+      <signup className={styles.signu}>
+        <section className={styles.section}>
+          <div className={styles.left}>
+            <div className={styles.imgBx}>
+              <div className={styles.img}>
+                <pic className={styles.pics1}></pic>
+                <div className={styles.txt}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the
+                </div>
+              </div>
+              <div className={styles.inputBx}>
+                <input type="submit" value="Individual" name="" />
+              </div>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.imgBx}>
+              <div className={styles.img}>
+                <pic className={styles.pics2}></pic>
+                <div className={styles.txt}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the
+                </div>
+              </div>
+              <div className={styles.inputBx}>
+                <input type="submit" value="Organisation" name="" />
+              </div>
+            </div>
+          </div>
+        </section>
+      </signup>
+    </div>
+  );
+}
