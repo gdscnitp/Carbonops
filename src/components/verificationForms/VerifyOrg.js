d2cc1d04cb125f474cd68c98eedb8bc5cef88932
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-01-10 20:10:45 +0530

    deals in waste checkbox made functional

--- src/components/verificationForms/VerifyOrg.js
+++ src/components/verificationForms/VerifyOrg.js
@@ -1,40 +1,47 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import styles from "./OrgForm.module.css";
 
 export default function VerifyOrg() {
+  const [dealsBool, setDealsBool] = useState(false);
 
-    const [ orgProfValues, setOrgProfValues] = useState({
-        organisationName:"",
-        typeOfOrganisation:"",
-        areaName: "",
-        cityName: "",
-        stateName: "",
-        countryName: "",
-        pincode: "",
-        wasteRequirements: "",
-        websiteLink: "",
-        linkedin: "",
-      });
-    
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setOrgProfValues({
-          ... orgProfValues,
-          [name]: value,
-        });
-      };
+  const [orgProfValues, setOrgProfValues] = useState({
+    organisationName: "",
+    typeOfOrganisation: "",
+    areaName: "",
+    cityName: "",
+    stateName: "",
+    countryName: "",
+    pincode: "",
+    wasteRequirements: "",
+    websiteLink: "",
+    linkedin: "",
+  });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log( orgProfValues)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setOrgProfValues({
+      ...orgProfValues,
+      [name]: value,
+    });
+  };
 
-      };
-    
-    
-    return (
-        <div>
-           <div className={styles.formBody}>
-      <h1 style={{color:"#54CCFA" , textAlign:"center" , marginBottom:"1rem"}}>Please complete your profile before proceeding further!</h1>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(orgProfValues);
+  };
+
+  return (
+    <div>
+      <div className={styles.formBody}>
+        <h1
+          style={{
+            color: "#54CCFA",
+            textAlign: "center",
+            marginBottom: "1rem",
+          }}
+        >
+          Please complete your profile before proceeding further!
+        </h1>
         <div className={styles.formOrg}>
           <form>
             <input
@@ -43,11 +50,11 @@ export default function VerifyOrg() {
               placeholder="Your Organisation Name *"
               id="organisationName"
               name="organisationName"
-              value={ orgProfValues.organisationName}
+              value={orgProfValues.organisationName}
               onChange={handleChange}
               required
               autoComplete="off"
-            /> 
+            />
             <br />
             <input
               className={`${styles.input} `}
@@ -55,11 +62,11 @@ export default function VerifyOrg() {
               placeholder="Your Area *"
               id="areaName"
               name="areaName"
-              value={ orgProfValues.areaName}
+              value={orgProfValues.areaName}
               onChange={handleChange}
               required
               autoComplete="off"
-            /> 
+            />
             <br />
             <input
               className={`${styles.input} `}
@@ -67,11 +74,11 @@ export default function VerifyOrg() {
               placeholder="Your City Name *"
               id="cityName"
               name="cityName"
-              value={ orgProfValues.cityName}
+              value={orgProfValues.cityName}
               onChange={handleChange}
               required
               autoComplete="off"
-            /> 
+            />
             <br />
             <input
               className={`${styles.input} `}
@@ -79,11 +86,11 @@ export default function VerifyOrg() {
               placeholder="Your State Name *"
               id="stateName"
               name="stateName"
-              value={ orgProfValues.stateName}
+              value={orgProfValues.stateName}
               onChange={handleChange}
               required
               autoComplete="off"
-            /> 
+            />
             <br />
             <input
               className={`${styles.input} `}
@@ -91,11 +98,11 @@ export default function VerifyOrg() {
               placeholder="Your Pincode *"
               id="pincode"
               name="pincode"
-              value={ orgProfValues.pincode}
+              value={orgProfValues.pincode}
               onChange={handleChange}
               required
               autoComplete="off"
-            /> 
+            />
             <br />
             <input
               className={`${styles.input} `}
@@ -103,36 +110,24 @@ export default function VerifyOrg() {
               placeholder="Your Country Name *"
               id="countryName"
               name="countryName"
-              value={ orgProfValues.countryName}
+              value={orgProfValues.countryName}
               onChange={handleChange}
               required
               autoComplete="off"
-            /> 
+            />
             <br />
-            
-            <input
-              className={`${styles.input} `}
-              type="text"
-              placeholder="Waste Requirements"
-              id="wasteRequirements"
-              name="wasteRequirements"
-              value={ orgProfValues.wasteRequirements}
-              onChange={handleChange}
-              required
-              autoComplete="off"
-            /> 
-            <br />
+
             <input
               className={`${styles.input} `}
               type="url"
               placeholder="Your Website Link *"
               id="websiteLink"
               name="websiteLink"
-              value={ orgProfValues.websiteLink}
+              value={orgProfValues.websiteLink}
               onChange={handleChange}
               required
               autoComplete="off"
-            /> 
+            />
             <br />
             <input
               className={`${styles.input} `}
@@ -144,46 +139,72 @@ export default function VerifyOrg() {
               onChange={handleChange}
               required
               autoComplete="off"
-            /> 
+            />
             <br />
 
-              <div className={styles.typesDeals}>
-
+            <div className={styles.typesDeals}>
               <label className={styles.eventType} htmlFor="eventType">
-              Type of Organisation <br />
-              <select
-                className={styles.select}
-                id="typeOfOrganisation"
-                name="typeOfOrganisation"
-                value={orgProfValues.typeOfOrganisation}
-                onChange={handleChange}
-                required
-                autoComplete="off"
-              >
-                <option value=""> Your Organisation type</option>
-                <option value="Cooperatives">Cooperatives</option>
-                <option value="Self help groups">Self help groups</option>
-                <option value="Private">Private </option>
-              </select>
-              <br />
-            </label>
+                Type of Organisation <br />
+                <select
+                  className={styles.select}
+                  id="typeOfOrganisation"
+                  name="typeOfOrganisation"
+                  value={orgProfValues.typeOfOrganisation}
+                  onChange={handleChange}
+                  required
+                  autoComplete="off"
+                >
+                  <option value=""> Your Organisation type</option>
+                  <option value="Cooperatives">Cooperatives</option>
+                  <option value="Self help groups">Self help groups</option>
+                  <option value="Private">Private </option>
+                </select>
+                <br />
+              </label>
               <br />
 
-              <label htmlFor="deals"> Deals ? 
-                  <input type="checkbox" />
+              <label htmlFor="deals">
+                Deals in waste products?
+                <input
+                  id="deals"
+                  type="checkbox"
+                  value={dealsBool}
+                  onClick={() => {
+                    setDealsBool(!dealsBool);
+                  }}
+                />
               </label>
               <br />
               <br />
-              </div>
+            </div>
 
-              <br />
+            <br />
 
-              <div className={styles.button}>
-                 <a onClick={handleSubmit}> Save Details</a> 
-              </div>
+            {dealsBool ? (
+              <>
+                <input
+                  className={`${styles.input} `}
+                  type="text"
+                  placeholder="Waste Requirements"
+                  id="wasteRequirements"
+                  name="wasteRequirements"
+                  value={orgProfValues.wasteRequirements}
+                  onChange={handleChange}
+                  required
+                  autoComplete="off"
+                />
+                <br />
+              </>
+            ) : (
+              ""
+            )}
+
+            <div className={styles.button}>
+              <a onClick={handleSubmit}> Save Details</a>
+            </div>
           </form>
         </div>
       </div>
-        </div>
-    )
+    </div>
+  );
 }
