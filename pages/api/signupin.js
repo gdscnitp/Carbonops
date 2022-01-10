84749cfbe43adca13c114555878d2dbdf902a5ca
Srijan Shovit <srijanshovit@gmail.com>
2022-01-10 11:31:55 +0530

    folder st changed+ verified schema + two api sign up routes

---
+++ pages/api/signupin.js
@@ -0,0 +1,31 @@
+
+import initDB from "../../helpers/db"
+import { sendSuccess,sendError } from "../../utilities/response-helpers"
+const {sendConfirmationMail} = require("./mailer")
+
+initDB() 
+
+export default async (req, res) => {
+    const {email,password,contact,dob} = req.body
+
+    try {
+        if ((!email || !password || !contact) || !dob ) {
+            sendError(res,"Please fill all fields",_,422)
+        }
+
+        const regUser =  await Individual.find({email})
+        const pUserAcc = await PendAcc.find({email})
+
+        if (regUser || pUserAcc) {
+            sendError(res,"User already exists",_,422)
+        }
+
+        const newAccount = await PendAcc({email,password,contact,dob})
+        await newAccount.save()
+        await sendConfirmationMail({toUser : newAccount.data, hash: newAccount.data._id})
+        sendSuccess(res,"Please check email for confirmation")
+    } catch (err) {
+        sendError(res,"Sorry",_,422)
+    }
+
+}
--- pages/api/signuporg.js
+++ pages/api/signuporg.js
@@ -6,21 +6,21 @@ const {sendConfirmationMail} = require("./mailer")
 initDB() 
 
 export default async (req, res) => {
-    const {email,password,contact,dob,organisationId} = req.body
+    const {email,password,contact,organisationId} = req.body
 
     try {
-        if ((!email || !password || !contact) || (!dob && !organisationId)) {
+        if ((!email || !password || !contact) ||  !organisationId){
             sendError(res,"Please fill all fields",_,422)
         }
 
-        const regUser = (dob) ? await Individual.find({email}) : await Org.find({email})
+        const regUser =  await Org.find({email})
         const pUserAcc = await PendAcc.find({email})
 
         if (regUser || pUserAcc) {
             sendError(res,"User already exists",_,422)
         }
 
-        const newAccount = await PendAcc({email,password,contact,dob,organisationId})
+        const newAccount = await PendAcc({email,password,contact,organisationId})
         await newAccount.save()
         await sendConfirmationMail({toUser : newAccount.data, hash: newAccount.data._id})
         sendSuccess(res,"Please check email for confirmation")
