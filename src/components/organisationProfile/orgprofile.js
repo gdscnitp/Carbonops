59079caae351bfc142ebe8f4735aa8027e51498e
MOHIT <mohitk.ug20.ece@nitp.ac.in>
2022-01-12 12:11:37 +0530

    productpage

--- src/components/organisationProfile/orgprofile.js
+++ src/components/organisationProfile/orgprofile.js
@@ -1,300 +1,330 @@
 import styles from "./orgprofile.module.css"
 import Image from "next/image";
 import undraw from "/public/images/organisationProfilee/undraw.png"
-import group from "/public/images/organisationProfilee/Group.png"
+
+import goomgle from "/public/images/organisationProfilee/goomgle.png"
 import Hack from "/public/images/organisationProfilee/Hack.png"
+import fb from "/public/images/organisationProfilee/fb.png"
+import twitt from "/public/images/organisationProfilee/twitt.png"
 
 
 const orgprofile = () => {
   return (
     <>
 
+          <div className={styles.dashb}>
+             Dashboard
+          </div>
+          <button  className={styles.event}>
+                  Create Events +
+          </button>
+
+      <div className={styles.cont1}>
+        <h2 className={styles.orgN}>Organisation Name</h2>
+        <span className={styles.welcome}> Welcome to our</span> <span className={styles.greenF}> <strong>Green Family</strong> </span>
+        <p className={styles.cont2}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, reprehenderit tempora impedit itaque pariatur repellat Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia temporibus, rem natus minima, placeat sapiente incidunt error magnam atque minus consequatur cupiditate! consequatur nostrum nisi officiis rem!</p>
+      </div>
 
 
-        <div className={styles.cont1}>
-          <h2 className={styles.orgN}>Organisation Name</h2>
-          <span className={styles.welcome}> Welcome to our</span> <span className={styles.greenF}> <strong>Green Family</strong> </span>
-          <p className={styles.cont2}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, reprehenderit tempora impedit itaque pariatur repellat Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia temporibus, rem natus minima, placeat sapiente incidunt error magnam atque minus consequatur cupiditate! consequatur nostrum nisi officiis rem!</p>
+      <div className={styles.cont3}>
+        <Image
+          src={undraw}
+          alt="Home Image"
+          width={250}
+          height={190}
+        />
+      </div>
+
+
+
+      <button className={styles.buton}>
+        Know more
+      </button>
+
+      <div className={styles.ordet}>
+        <h2> <strong>Organisation Details</strong></h2>
+      </div>
+
+
+      <div className={styles.bcc}>
+
+
+        <div className={styles.torg}>
+
+          <h3 className={styles.torgg}>Type Of Org </h3>
+          <h3 className={styles.bol}> Based in *Location*</h3>
+          <br />
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste maiores alias quas esse assumenda odit velit facilis laboriosam, similique ipsa rem labore iure blanditiis illum ullam aliquam ipsam dolore temporibus quaerat exercitationem suscipit, veritatis quis tempore delectus! Autem, amet in. Doloribus modi odit maxime ab sit debitis, neque iste numquam!</p>
+          <br />
+          <hr className={styles.hr} />
+          <br />
+          <p className={styles.bol}>
+            Contact Us
+          </p>
+          <br />
+          <div className={styles.cont4}>
+            <Image
+              src={goomgle}
+              alt="Home Image"
+              width={40}
+              height={32}
+            />
+          </div>
+          <div className={styles.cont5}>
+            <Image
+              src={fb}
+              alt="Home Image"
+              width={37}
+              height={32}
+            />
+          </div>
+          <div className={styles.cont6}>
+            <Image
+              src={twitt}
+              alt="Home Image"
+              width={50}
+              height={32}
+            />
+          </div>
         </div>
+      </div>
 
 
-        <div className={styles.cont3}>
+
+      <div className={styles.products}>
+        <h2>Our Products</h2>
+      </div>
+
+
+      {/* ---------------------------------------------------------------------- */}
+
+
+
+
+      <div className={styles.services1}>
+
+
+
+        <div className={styles.box1}>
           <Image
-              src={undraw}
-            alt="Home Image"
-            width={250}
-            height={190}
+            src={Hack}
+            alt="Hack Image"
+            width={320}
+            height={130}
           />
-        </div>
 
+          <h2 className={styles.hsecondary}>Product Name</h2>
+          <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
+            . Quas auem fugiat dolorme <span className={styles.descr}> *DESCRIPTION*</span></p>
 
+          <div className={styles.card}>
 
-        <div className={styles.buton}>
-          Know more
-        </div>
-
-        <div className={styles.ordet}>
-          <h2> <strong>Organisation Details</strong></h2>
-        </div>
-
-
-        <div className={styles.bcc}>
-
-
-          <div className={styles.torg}>
-
-            <h3 className={styles.torgg}>Type Of Org </h3>
-            <h3 className={styles.bol}> Based in *Location*</h3>
-            <br />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste maiores alias quas esse assumenda odit velit facilis laboriosam, similique ipsa rem labore iure blanditiis illum ullam aliquam ipsam dolore temporibus quaerat exercitationem suscipit, veritatis quis tempore delectus! Autem, amet in. Doloribus modi odit maxime ab sit debitis, neque iste numquam!</p>
-             <br />
-            <hr className={styles.hr} />
-           <br />
-            <p className={styles.bol}>
-              Contact Us
-            </p>
-            <br />
-            <div className={styles.cont4}>
-              <Image
-                src={group}
-                alt="Home Image"
-                width={150}
-                height={39}
-              />
+            <div className={styles.btn1}>
+              $45
             </div>
+
+            <div className={styles.btn2}>
+              Buy Now
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div className={styles.box1}>
+          <Image
+            src={Hack}
+            alt="Hack Image"
+            width={320}
+            height={130}
+          />
+          <h2 className={styles.hsecondary}>Product Name</h2>
+          <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
+            . Quas autem fugiat dolore <span className={styles.descr}> *DESCRIPTION*</span></p>
+
+          <div className={styles.card}>
+
+            <div className={styles.btn1}>
+              $45
+            </div>
+
+            <div className={styles.btn2}>
+              Buy Now
+            </div>
+
+          </div>
+
+        </div>
+
+
+        <div className={styles.box1}>
+          <Image
+            src={Hack}
+            alt="Hack Image"
+            width={320}
+            height={130}
+          />
+          <h2 className={styles.hsecondary}>Product Name</h2>
+          <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
+            . Quas autem fugiat dolore  <span className={styles.descr}> *DESCRIPTION*</span></p>
+
+          <div className={styles.card}>
+
+            <div className={styles.btn1}>
+              $45
+            </div>
+
+            <div className={styles.btn2}>
+              Buy Now
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className={styles.box1}>
+          <Image
+            src={Hack}
+            alt="Hack Image"
+            width={320}
+            height={130}
+          />
+          <h2 className={styles.hsecondary}>Product Name</h2>
+          <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
+            . Quas autem fugiat dolore  <span className={styles.descr}> *DESCRIPTION*</span></p>
+
+          <div className={styles.card}>
+
+            <div className={styles.btn1}>
+              $45
+            </div>
+
+            <div className={styles.btn2}>
+              Buy Now
+            </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+
+
+      {/* ------------------------------------------------------------------------------------- */}
+
+
+
+      <div className={styles.services2}>
+        <div className={styles.box2}>
+          <Image
+            src={Hack}
+            alt="Hack Image"
+            width={320}
+            height={130}
+          />
+          <h2 className={styles.hsecondary}>Product Name</h2>
+          <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
+            . Quas auem fugiat dolorse  <span className={styles.descr}> *DESCRIPTION*</span></p>
+
+          <div className={styles.card}>
+
+            <div className={styles.btn1}>
+              $45
+            </div>
+
+            <div className={styles.btn2}>
+              Buy Now
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className={styles.box2}>
+          <Image
+            src={Hack}
+            alt="Hack Image"
+            width={320}
+            height={130}
+          />
+          <h2 className={styles.hsecondary}>Product Name</h2>
+          <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
+            . Quas autem fugiat dolore  <span className={styles.descr}> *DESCRIPTION*</span></p>
+
+          <div className={styles.card}>
+
+            <div className={styles.btn1}>
+              $45
+            </div>
+
+            <div className={styles.btn2}>
+              Buy Now
+            </div>
+
           </div>
         </div>
 
 
+        <div className={styles.box2}>
+          <Image
+            src={Hack}
+            alt="Hack Image"
+            width={320}
+            height={130}
+          />
+          <h2 className={styles.hsecondary}>Product Name</h2>
+          <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
+            . Quas autem fugiat dolore  <span className={styles.descr}> *DESCRIPTION*</span>.</p>
 
-        <div className={styles.products}>
-          <h2>Our Products</h2>
+          <div className={styles.card}>
+
+            <div className={styles.btn1}>
+              $45
+            </div>
+
+            <div className={styles.btn2}>
+              Buy Now
+            </div>
+
+          </div>
         </div>
 
+        <div className={styles.box2}>
+          <Image
+            src={Hack}
+            alt="Hack Image"
+            width={320}
+            height={130}
+          />
+          <h2 className={styles.hsecondary}>Product Name</h2>
+          <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
+            . Quas autem fugiat dolore  <span className={styles.descr}> *DESCRIPTION*</span></p>
 
-        {/* ---------------------------------------------------------------------- */}
+          <div className={styles.card}>
 
-        <section className={styles.servicescontainer}>
-
-
-          <div className={styles.services}>
-            <div className={styles.box}>
-              <Image
-                src={Hack}
-                alt="Hack Image"
-                width={320}
-                height={130}
-              />
-              
-              <h2 className={styles.hsecondary}>Product Name</h2>
-              <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
-                . Quas auem fugiat dolore quae.</p>
-
-              <div className={styles.card}>
-
-                <div className={styles.btn1}>
-                  $45
-                </div>
-
-                <div className={styles.btn2}>
-                  Buy Now
-                </div>
-
-              </div>
-
-
+            <div className={styles.btn1}>
+              $45
             </div>
 
-            <div className={styles.box}>
-              <Image
-                 src={Hack}
-                alt="Hack Image"
-                width={320}
-                height={130}
-              />
-              <h2 className={styles.hsecondary}>Product Name</h2>
-              <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
-                . Quas autem fugiat dolore quae.</p>
-
-              <div className={styles.card}>
-
-                <div className={styles.btn1}>
-                  $45
-                </div>
-
-                <div className={styles.btn2}>
-                  Buy Now
-                </div>
-
-              </div>
-
-            </div>
-
-
-            <div className={styles.box}>
-              <Image
-                src={Hack}
-                alt="Hack Image"
-                width={320}
-                height={130}
-              />
-              <h2 className={styles.hsecondary}>Product Name</h2>
-              <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
-                . Quas autem fugiat dolore quae.</p>
-
-              <div className={styles.card}>
-
-                <div className={styles.btn1}>
-                  $45
-                </div>
-
-                <div className={styles.btn2}>
-                  Buy Now
-                </div>
-
-              </div>
-
-            </div>
-
-            <div className={styles.box}>
-              <Image
-                src={Hack}
-                alt="Hack Image"
-                width={320}
-                height={130}
-              />
-              <h2 className={styles.hsecondary}>Product Name</h2>
-              <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
-                . Quas autem fugiat dolore quae.</p>
-
-              <div className={styles.card}>
-
-                <div className={styles.btn1}>
-                  $45
-                </div>
-
-                <div className={styles.btn2}>
-                  Buy Now
-                </div>
-
-              </div>
-
+            <div className={styles.btn2}>
+              Buy Now
             </div>
 
           </div>
-        </section>
 
 
+        </div>
 
-        {/* ------------------------------------------------------------------------------------- */}
-
-        <section className={styles.servicescontainer2}>
-
-          <div className={styles.services}>
-            <div className={styles.box}>
-              <Image
-                 src={Hack}
-                alt="Hack Image"
-                width={320}
-                height={130}
-              />
-              <h2 className={styles.hsecondary}>Product Name</h2>
-              <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
-                . Quas auem fugiat dolore quae.</p>
-
-              <div className={styles.card}>
-
-                <div className={styles.btn1}>
-                  $45
-                </div>
-
-                <div className={styles.btn2}>
-                  Buy Now
-                </div>
-
-              </div>
-
-            </div>
-
-            <div className={styles.box}>
-              <Image
-                src={Hack}
-                alt="Hack Image"
-                width={320}
-                height={130}
-              />
-              <h2 className={styles.hsecondary}>Product Name</h2>
-              <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
-                . Quas autem fugiat dolore quae.</p>
-
-              <div className={styles.card}>
-
-                <div className={styles.btn1}>
-                  $45
-                </div>
-
-                <div className={styles.btn2}>
-                  Buy Now
-                </div>
-
-              </div>
-            </div>
+      </div>
 
 
-            <div className={styles.box}>
-              <Image
-                src={Hack}
-                alt="Hack Image"
-                width={320}
-                height={130}
-              />
-              <h2 className={styles.hsecondary}>Product Name</h2>
-              <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
-                . Quas autem fugiat dolore quae.</p>
-
-              <div className={styles.card}>
-
-                <div className={styles.btn1}>
-                  $45
-                </div>
-
-                <div className={styles.btn2}>
-                  Buy Now
-                </div>
-
-              </div>
-            </div>
-
-            <div className={styles.box}>
-              <Image
-               src={Hack}
-                alt="Hack Image"
-                width={320}
-                height={130}
-              />
-              <h2 className={styles.hsecondary}>Product Name</h2>
-              <p className={styles.center}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, eaque. Aliquam dolor
-                . Quas autem fugiat dolore quae.</p>
-
-              <div className={styles.card}>
-
-                <div className={styles.btn1}>
-                  $45
-                </div>
-
-                <div className={styles.btn2}>
-                  Buy Now
-                </div>
-
-              </div>
-
-
-            </div>
-
-          </div>
-        </section>
-
-
+      <button className={styles.viewmore}>
+        View more
+      </button>
 
     </>
   )
