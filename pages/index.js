2cd06cdad2dcc0378bd8d04442fe55a4a40eb3dd
amisha <amishab.ug20.ce@nitp.ac.in>
2022-01-16 19:54:06 +0530

    about us

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,25 @@
 import Head from "next/head";
-import { useEffect } from "react";
-import logger from "../helpers/logger";
 import Navbar from "../src/components/navbar/Navbar";
 import HomePage from "../src/components/homePage/HomePage";
 import Footer from "../src/components/footer/Footer";
 import styles from "../styles/Home.module.css";
+import Aboutus from '../src/components/AboutUs/Aboutus'
 
 export default function Home() {
-  logger.info("Home");
-  useEffect(() => {
-    logger.debug("Home mounted");
-  }, []);
   return (
-    <div className={styles.container}>
+     <div className={styles.container}>
       <Head>
         <title>Environment Awareness App</title>
         <meta name="description" content="" />
       </Head>
-
-      <Navbar action1="" action2="Login" buttonText1="SignUp" buttonText2="" />
+      <Aboutus />
+      <Navbar action1="" action2="Login" buttonText="SignUp" />
+     
       {/* <div className={styles.circle}></div> */}
       <main className={styles.main}>
         <HomePage />
       </main>
-
+     
       <footer>
         <Footer />
       </footer>
