6b19ebe18ca3d6141ce1a62c2451e0b7a68efb9a
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-01-21 12:39:42 +0530

    connected to form

--- pages/create-events.js
+++ pages/create-events.js
@@ -2,10 +2,22 @@ import CreateEvents from '../src/components/events/CreateEvents'
 import Navbar from '../src/components/navbar/Navbar'
 
 export default function Events() {
+    async function onAddEventHandler(values) {
+       const response = await fetch('/api/created-events',{
+        method:'POST',
+        body:JSON.stringify(values),
+        headers:{
+            'Content-Type' :'application/json'
+        }
+    })
+        const data = await response.json();
+        console.log(data);
+    }
+
     return (
         <>
         <Navbar action1="Dashboard"  action2="Events" buttonText1="" buttonText2=""/>
-        <CreateEvents />
+        <CreateEvents onAddEvent={onAddEventHandler}/>
         </>
     )
 }
