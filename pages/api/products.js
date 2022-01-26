32644abdfb5dba2a7d131f9c54ac87cddc2af1ed
shivam tyagi <shivamt.ug19.ec@nitp.ac.in>
2022-01-26 16:55:28 +0530

    deletion updated

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,27 +1,58 @@
 //A file to check all APIS
-import mongoose from 'mongoose'
-import initDB from "../../helpers/db"
-import { sendSuccess,sendError } from "../../utilities/response-helpers"
-var proSc =require("../../models/Product")
+import mongoose from "mongoose";
+import initDB from "../../helpers/db";
+import { sendSuccess, sendError } from "../../utilities/response-helpers";
+var proSc = require("../../models/Product");
 // console.log(proSc)
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        console.log(req.body)
+  if (req.method === "POST") {
+    console.log(req.body);
+    try {
+      // for addition of products
 
-        // for products
-        var {organisationId,productName,price,description,productImage,rating,stockUnits,isRecyclable,productUrl} = req.body;
+      var userId = {
+        id: { $eq: req.body.id },
+      };
+      var {
+        organisationId,
+        productName,
+        price,
+        description,
+        productImage,
+        rating,
+        stockUnits,
+        isRecyclable,
+        productUrl,
+      } = req.body;
 
-        initDB();
-      var prod=new proSc({organisationId,productName,price,description,productImage,rating,stockUnits,isRecyclable,productUrl})
+      initDB();
+      var prod = new proSc({
+        organisationId,
+        productName,
+        price,
+        description,
+        productImage,
+        rating,
+        stockUnits,
+        isRecyclable,
+        productUrl,
+      });
 
-         console.log(prod)
-          await prod.save();
-        mongoose.connection.close();
-        
-         return sendSuccess(res,prod);
-    } 
-    else {
-        return sendSuccess(res,{"message": "This works"});
+      console.log(prod);
+      await prod.save();
+
+      let info = await proSc.deleteOne({ _id: { $eq: req.body.id } });
+      //info is delete info in form of delete count
+      console.log(info);
+
+      mongoose.connection.close();
+
+      return sendSuccess(res, prod);
+    } catch (error) {
+      console.log(error.message);
     }
+  } else {
+    return sendSuccess(res, { message: "This works" });
   }
+}
