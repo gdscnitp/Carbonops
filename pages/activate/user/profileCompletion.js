ffb6a8e3b44073975a77335b738100755f037844
priyakumari02 <77202746+priyakumari02@users.noreply.github.com>
2022-01-28 17:29:42 +0530

    profile Completion

---
+++ pages/activate/user/profileCompletion.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './profileCompletion.module.css' 
+import { TiTick } from 'react-icons/ti';
+
+export default function profileCompletion({ id, profHref }) {
+  return (
+    <>
+      <div className={styles.profilePage}>
+        <div className={styles.container}>
+          <div className={styles.tick}>
+          <TiTick style={{ fontSize: '55px' }}/>
+          </div> 
+          <div className={styles.text}>
+          {id} Your account is <strong>Verified!</strong> . Please complete profile here{' '}
+          </div>
+          <Link href={`../../${profHref}/${id}`}>
+            <a className={styles.button}>Profile Comp </a>
+          </Link>
+        </div> 
+      </div>
+    </>
+  );
+}
