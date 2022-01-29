68315b9f509dfc87b168348a3d2dadbf46927d15
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-01-29 18:35:56 +0530

    event duplication error handling added

--- pages/api/created-events.js
+++ pages/api/created-events.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose'
 import { sendSuccess,sendError } from "../../utilities/response-helpers"
 import EventSc from "../../models/Event";
 
+initDB();
   
-
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     console.log(req.body)
-
+    
     const {eventDate,eventTime,eventDuration,eventPoster,eventName,eventType,targetAudience,eventDescription,participantsLimit,isOffline,eventLocation,enquiryDetails,onOrgModel}=req.body;
 
       if (!eventName || !eventType || !eventDate || !eventTime || !eventDuration || !eventPoster || !targetAudience || !eventDescription || !participantsLimit  || !enquiryDetails || !onOrgModel) {
@@ -18,18 +18,27 @@ export default async function handler(req, res) {
     if (isOffline===true &&  eventLocation.length<=0){
       return sendError(res, "Please provide event's location",11,404);
     }
-    initDB();
-    const item=new EventSc({
-      eventName:eventName,eventType:eventType,eventDetails:{date:eventDate,time:eventTime ,duration:eventDuration , poster:eventPoster},targetAudience:targetAudience,eventDescription:eventDescription,participantsLimit:participantsLimit,isOffline:isOffline,enquiryDetails:enquiryDetails,onOrgModel,location:eventLocation,
-    })
 
-    eventLocation ? item['eventLocation'] = eventLocation : "";
-          console.log(item)
-          await item.save();
-          //  console.log(item);
-         // mongoose.connection.close();
-          return sendSuccess(res,item);
+   //checking if event already exists in database
+   const nameExists = await EventSc.findOne({eventName:eventName})
+      console.log(nameExists);
+       if (!nameExists) {
 
+          const item=new EventSc({
+            eventName:eventName,eventType:eventType,eventDetails:{date:eventDate,time:eventTime ,duration:eventDuration , poster:eventPoster},targetAudience:targetAudience,eventDescription:eventDescription,participantsLimit:participantsLimit,isOffline:isOffline,enquiryDetails:enquiryDetails,onOrgModel,location:eventLocation,
+          })
+
+          eventLocation ? item['eventLocation'] = eventLocation : "";
+                console.log(item)
+                await item.save();
+                //  console.log(item);
+              // mongoose.connection.close();
+                return sendSuccess(res,item);
+      }
+      else{
+      return sendError(res, "Event with this name is already registered",10,403);
+        
+      }
   }
 
   else {
