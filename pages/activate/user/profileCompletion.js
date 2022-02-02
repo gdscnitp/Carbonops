4b44b286d6f36248eb4387eb9b47388ae4b0ba18
shivam tyagi <shivamt.ug19.ec@nitp.ac.in>
2022-02-02 23:35:26 +0530

    updates

--- pages/activate/user/profileCompletion.js
+++ pages/activate/user/profileCompletion.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Link from 'next/link';
-import styles from './profileCompletion.module.css' 
-import { TiTick } from 'react-icons/ti';
+import React from "react";
+import Link from "next/link";
+import styles from "./profileCompletion.module.css";
+import { TiTick } from "react-icons/ti";
 
 export default function ProfileCompletion({ id, profHref }) {
   return (
@@ -9,15 +9,16 @@ export default function ProfileCompletion({ id, profHref }) {
       <div className={styles.profilePage}>
         <div className={styles.container}>
           <div className={styles.tick}>
-          <TiTick style={{ fontSize: '55px' }}/>
-          </div> 
+            <TiTick style={{ fontSize: "55px" }} />
+          </div>
           <div className={styles.text}>
-          {id} Your account is <strong>Verified!</strong> . Please complete profile here{' '}
+            {id} Your account is <strong>Verified!</strong> . Please complete
+            profile here{" "}
           </div>
           <Link href={`../../${profHref}/${id}`}>
             <a className={styles.button}>Profile Comp </a>
           </Link>
-        </div> 
+        </div>
       </div>
     </>
   );
