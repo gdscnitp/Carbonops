8102b0b1a14187b5df5a5736dd362a9e56609c51
PalakKumari <palakkumari404@gmail.com>
2022-02-10 01:08:54 +0530

    Update footprint-calculator.js

--- pages/api/footprint-calculator.js
+++ pages/api/footprint-calculator.js
@@ -5,7 +5,7 @@ export default async function handler(req,res)
 {
    if(req.method==='POST')
    {
-       const {members,houseSize,meat,packagedFood}=req.body;
+       const {members,houseSize,meat,packagedFood,dishWasher,washingMachine, householdPurchases, garbageCans,recycles, itemsRecycled}=req.body;
        //validate the input
        var score=0;
        //for members
@@ -52,6 +52,7 @@ export default async function handler(req,res)
            }
        }
 
+       //************************** for food ****************
       if(meat)
       {
           if(meat=="daily")
@@ -71,6 +72,9 @@ export default async function handler(req,res)
               score+=2;
           }
       }
+
+
+      //*********************************** for packaged food**************************
       if(packagedFood)
       {
           if(packagedFood=="frequently")
@@ -79,13 +83,112 @@ export default async function handler(req,res)
           }
           else if(packagedFood=="balanced")
           {
-              score+=12;
+              score+=6;
           }
           else if(packagedFood=="no")
           {
               score+=2;
           }
       }
+
+
+      //***********************//for water Consumption***********************
+      if(dishWasher)
+      {
+         if(dishWasher=="1 to 3 times")
+         {
+          score+=1;
+         }
+         else if(dishWasher=="4 to 9 times")
+         {
+             score+=2;
+         }
+         else if(dishWasher=="more than 9 times")
+         {
+             score+=3;
+         }
+         //also add a no dishwasher and no washing machine option
+      }
+
+         //***********************  //for washing machine*************************
+      if(washingMachine)
+      {
+        if(washingMachine=="1 to 3 times")
+        {
+         score+=1;
+        }
+        else if(washingMachine=="4 to 9 times")
+        {
+            score+=2;
+        }
+        else if(washingMachine=="more than 9 times")
+        {
+            score+=3;
+        }
+      }
+
+
+      //******************************//for household purchases.************************
+      if(householdPurchases)
+      {
+          if(householdPurchases>7)
+          {
+              score+=10;
+          }
+          else if(householdPurchases>=5 && householdPurchases<=7)
+          {
+              score+=8;
+          }
+          else if(householdPurchases>=3 && householdPurchases<=5)
+          {
+              score+=6;
+          }
+          else if(householdPurchases<3)
+          {
+              score+=4;
+          }
+          //if you purchase almost nothing or only second hand items then add 2 points
+      }
+
+    //  ******************************for garbage cans filled ************************
+      if(garbageCans)
+      {
+          if(garbageCans>=4)
+          {
+              score+=50;
+          }
+          else if(garbageCans>2 && garbageCans<=3)
+          {
+              score+=40;
+          }
+          else if(garbageCans>1 && garbageCans<=2)
+          {
+              score+=30;
+          }
+          else if(garbageCans==1)
+          {
+              score+=20;
+          }
+          else if(garbageCans<1)
+          {
+              score+=5;
+          }
+      }
+      /****************///for recycling items ***************
+      if(recycles===false)
+      {
+          score+=24;
+      }
+      else 
+      {
+          score+=24;
+          var noOfRecycled=itemsRecycled.length;//items recycled is an array that can have recyclable items 
+          //such as glass, plastic, paper, aluminium steel, food waste
+          score-=4*noOfRecycled;
+      }
+
+
+      return sendSuccess(res,score);
       
 
    }
