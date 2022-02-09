e2ecaeef6f176369e82d5d299fb93add078d890d
PalakKumari <palakkumari404@gmail.com>
2022-02-09 23:02:55 +0530

    Update footprint-calculator.js

--- pages/api/footprint-calculator.js
+++ pages/api/footprint-calculator.js
@@ -1,13 +1,13 @@
-import { MdCardMembership } from "react-icons/md"
-import {sendError,sendSuccess} from "../../utilities/response-helpers"
 
+import {sendError,sendSuccess} from "../../utilities/response-helpers"
+//the higher the score, more co2 produced.
 export default async function handler(req,res)
 {
    if(req.method==='POST')
    {
-       const {members,houseSize,food}=req.body;
+       const {members,houseSize,meat,packagedFood}=req.body;
        //validate the input
-       let score=0;
+       var score=0;
        //for members
        if(members)
        {
@@ -51,6 +51,41 @@ export default async function handler(req,res)
                score+=2;
            }
        }
+
+      if(meat)
+      {
+          if(meat=="daily")
+          {
+              score+=10;
+          }
+          else if(meat=="weekly")
+          {
+              score+=8;
+          }
+          else if(meat=="vegetarian")
+          {
+              score+=4;
+          }
+          else if(meat=="vegan")
+          {
+              score+=2;
+          }
+      }
+      if(packagedFood)
+      {
+          if(packagedFood=="frequently")
+          {
+              score+=12;
+          }
+          else if(packagedFood=="balanced")
+          {
+              score+=12;
+          }
+          else if(packagedFood=="no")
+          {
+              score+=2;
+          }
+      }
       
 
    }
