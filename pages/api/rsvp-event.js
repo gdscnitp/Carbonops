7f220d8c658d3a29e869cd7a6e0791166c6bfaa0
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-02-20 12:19:42 +0530

    Merge remote-tracking branch 'upstream/main'

---
+++ pages/api/rsvp-event.js
@@ -0,0 +1,72 @@
+import initDB from "../../helpers/db";
+import mongoose from "mongoose";
+import { sendSuccess, sendError } from "../../utilities/response-helpers";
+import regEventSchema from "../../models/EventRegistered";
+import Indiv from "../../models/Individual"
+
+initDB();
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body);
+
+    const {
+      individualId,
+      eventId,
+      name,
+      mailId,
+      phoneNumber,
+    } = req.body;
+
+    if (
+      !individualId ||
+      !eventId ||
+      !name ||
+      !mailId ||
+      !phoneNumber
+    ) {
+      return sendError(res, "All values not found", 2, 404);
+    }
+
+    var indivMail = {
+      mail: { $eq: req.body.mailId },
+    };
+
+    //checking if user already exists in database
+    const mailExists = await Indiv.findOne({ email: indivMail.mail });
+    // console.log(mailExists,"mailExists");
+
+    if(mailExists){
+      const item = new regEventSchema({
+      individualId:individualId,
+      eventId:eventId,
+      name:name,
+      mailId:mailId,
+      phoneNumber:phoneNumber,
+      });
+
+      console.log(item);
+      await item.save();
+      //  console.log(item);
+      // mongoose.connection.close();
+      return sendSuccess(res, item);
+
+  } 
+  else{
+    return sendError(res, "Mail ID not found", 2, 404);
+  }
+}
+  else {
+    return sendError(res, "Bad request(NOT POST)", 2, 400);
+  }
+}
+
+
+// {
+//   "individualId":"1235468",
+//    "eventId":"987654654",
+//    "name":"Pushkar",
+//    "mailId":"paladfdfk234@gmail.com",
+//    "phoneNumber":12354997
+// }
+
