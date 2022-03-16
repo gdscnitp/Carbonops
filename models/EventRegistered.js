90f394d20c175c0c6f96e156b810eab5edd950ce
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-03-16 14:08:20 +0530

    modal updated

--- models/EventRegistered.js
+++ models/EventRegistered.js
@@ -1,43 +1,61 @@
 const mongoose = require("mongoose");
 // var {Indiv} = require("./Individual")
-const {isEmail} = require("validator");
+const { isEmail } = require("validator");
 
-const regEventSchema = new mongoose.Schema({
-    individualId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Individual'
+const regEventSchema = new mongoose.Schema(
+  {
+    individualId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Individual",
     },
-    eventId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'event'
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "event",
     },
-    name:{
-        type:String,
-        required:true,
-        trim:true,
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    mailId:{
-        type: String,
-        required: true,
-        trim:true,
-        validate: [ isEmail, 'Please provide a valid email ID' ]
+    mailId: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: [isEmail, "Please provide a valid email ID"],
     },
-    phoneNumber:{
-        type: Number,
-        required: true,        
+    phoneNumber: {
+      type: Number,
+      required: true,
     },
-},{
-    timestamps:true
-})
-
-function getDemo () {
-    
-    const iSchema = regEventSchema;
-    
-    if (mongoose.models && mongoose.models.eventreg) return mongoose.models.eventreg
-    
-    return mongoose.model('eventreg', iSchema)
+    eventName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    eventDate: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    eventTime: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
   }
-  const EventRegSc= getDemo()
-  module.exports= EventRegSc;
-  console.log(EventRegSc)
+);
+
+function getDemo() {
+  const iSchema = regEventSchema;
+
+  if (mongoose.models && mongoose.models.eventreg)
+    return mongoose.models.eventreg;
+
+  return mongoose.model("eventreg", iSchema);
+}
+const EventRegSc = getDemo();
+module.exports = EventRegSc;
+console.log(EventRegSc);
