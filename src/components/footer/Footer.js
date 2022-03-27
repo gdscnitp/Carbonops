a15ae0694d2e735eea16c774ac744fc3e3a302d5
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-03-27 18:25:29 +0530

    footer updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,13 +2,21 @@ import React from "react";
 import Image from "next/image";
 import styles from "./Footer.module.css";
 import Link from "next/link";
+import Carbonops from "/public/images/homePage/Carbonops.png";
 
 export default function Footer() {
   return (
     <>
       <div className={styles.container}>
         <div className={`${styles.footerLogo}`}>
-       <Link href="/">LOGO HERE</Link>
+       <Link href="/">
+       <Image
+       src={Carbonops}
+                  alt="Social icons"
+                  height={60}
+                  width={210}
+      />
+       </Link>
         </div>
         <div className={`${styles.footerAbout}`}>
           <Link href="./aboutus"> ABOUT US </Link>
@@ -19,18 +27,31 @@ export default function Footer() {
         <div className={`${styles.TandC}`}>Terms & Conditions</div>
         <div className={`${styles.copyRights}`}>Copyrights &copy; Env_App</div>
         <div className={`${styles.socialMedia}`}>
-          {["Google", "Facebook", "Twitter"].map((images) => {
-            return (
-              <div key={images} className={styles.socialIcons}>
+ 
+              <div className={styles.socialIcons}>
                 <Image
-                  src={`/images/homePage/${images}.png`}
+                  src={`/images/homePage/Google.png`}
                   alt="Social icons"
                   height={40}
                   width={40}
                 />
               </div>
-            );
-          })}
+              <div  className={styles.socialIcons}>
+                <Image
+                  src={`/images/homePage/Facebook.png`}
+                  alt="Social icons"
+                  height={40}
+                  width={40}
+                />
+              </div>
+              <div className={styles.socialIcons}>
+                <Image
+                  src={`/images/homePage/Twitter.png`}
+                  alt="Social icons"
+                  height={40}
+                  width={40}
+                />
+              </div>
         </div>
       </div>
     </>
